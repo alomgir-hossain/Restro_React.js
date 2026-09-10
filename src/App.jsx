19d51a156e0937340be1ab1f4903f29a7c53bef6
MD Alomgir Hossain <alomgir.hossain.official.dev@gmail.com>
2026-09-10 12:09:19 +0600

    about section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import About from './sections/About'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <HeroSection/>
+      <About/>
     </>
   )
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,44 @@
+
+import Animated from '../components/Animated'
+
+const About = () => {
+  return (
+    <section id='about' className='px-auto mt-44'>
+      <div className='flex flex-col md:flex-row gap-14 md:gap-18 max-w-7xl mx-auto'>
+        {/* Left side: Main image */}
+        <Animated scale={0.8} y={0} >
+          <img src='/assets/about.png' alt='Dish' className='max-w-137 w-full h-full object-cover rounded-3xl' />
+        </Animated>
+
+        {/* Right side: Content details */}
+        <div>
+          <Animated scale={0.8} y={0} className="flex items-center gap-2">
+            <img src='/assets/iconL.png' alt='iconLeft' />
+            <span>Crafted with Passion</span>
+            <img src='/assets/iconR.png' alt='iconRight' />
+          </Animated>
+
+          <Animated>
+            <h2 className='mt-5 text-4xl md:text-5xl text-balance'>
+              Experience dining beyond expectations
+            </h2>
+          </Animated>
+
+          <Animated delay={0.2}>
+            <p className='mt-4.5 text-zinc-600 max-w-sm'>We combine fresh local ingredients, creative recipies and elegant presentation to deliver a memorable expreience with every visit.</p>
+          </Animated>
+
+          <Animated className="mt-9 bg-orange-500 text-white p-2 pr-8 rounded-lg flex items-center gap-3 w-fit">
+            <img src='/assets/about.png' alt='Bistro Royal Location Preview' className='size-15 rounded-lg-boject-cover shrink-0' />
+            <div className='flex flex-col gap-2'>
+              <p className='font-medium'>Bistro Royal, NY</p>
+              <a href="#">View on Map</a>
+            </div>
+          </Animated>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
