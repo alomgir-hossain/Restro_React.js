f5f04d7c2b38ad80aa613650cb303f70a73bcf4c
MD Alomgir Hossain <alomgir.hossain.official.dev@gmail.com>
2026-09-10 12:19:26 +0600

    useEffect dependency array missed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         };
         window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll)
-    })
+    },[])
 
     return (
         <>
