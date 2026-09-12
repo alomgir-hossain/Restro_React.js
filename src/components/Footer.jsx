1a0947984d3f91f3f0ec74465b9f7d1a3cf6d692
alomgir <alomgir.hossain.official.dev@gmail.com>
2026-09-12 16:11:30 +0600

    Footer Section complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
-import { quickLinks, socialLinks } from '../data/data'
+import { quickLinks, sitemapLinks, socialLinks } from '../data/data'
 import Animated from './Animated'
+import { Mail, Phone } from 'lucide-react'
 
 const Footer = () => {
     return (
@@ -43,16 +44,50 @@ const Footer = () => {
                     </div>
 
                     {/* Column 3 - Get in Touch */}
-                    
+                    <div>
+                        <p className='font-medium mb-5'>Get In Touch</p>
+                        <div className='space-y-2'>
+                            <Animated>
+                                <a href="mailto:alomgir@gmail.com" className='flex items-center gap-1 text-zinc-600 hover:text-zinc-500'>
+                                    < Mail size={16} className='shrink-0' />
+                                    alomgir@gamil.com
+                                </a>
+                            </Animated>
+                            <Animated delay={0.2}>
+                                <a href="tel:01313627494" className='flex items-center gap-1 text-zinc-600 hover:text-zinc-500'>
+                                    < Phone size={16} className='shrink-0' />
+                                    01313627494
+                                </a>
+                            </Animated>
+                        </div>
+                    </div>
 
                     {/* Column 4 - Sitemap */}
+                    <div>
+                        <p className='font-medium mb-5'>Sitemap</p>
+                        <div className='flex flex-col gap-2.5'>
+                            {sitemapLinks.map((link, index) => (
+                                <Animated key={link.name} delay={index * 0.05}>
+                                    <a href={link.href} className='text-zinc-600'>
+                                        {link.name}
+                                    </a>
+                                </Animated>
+                            ))}
+                        </div>
+                    </div>
 
                 </div>
 
                 {/* Bottom Bar */}
-                <div>
-
+                <div className='border-t text-zinc-500 border-slate-200 py-4.5 flex justify-between items-center'>
+                    <p>&copy; 2026. All Right Reserved.</p>
+                    <p>Designed By <a href="https://alomgir.billionaire.dev">Alomgir Hossain</a></p>
                 </div>
+            </div>
+
+            {/* Watermark Logo Backdrop */}
+            <div className='absolute inset-0 text-center select-none -z-1 pointer-events-none'>
+                <span className='text-[300px] tracking-wide font-urbanist font-semibold text-zinc-100/70'>Restro</span>
             </div>
         </footer>
     )
